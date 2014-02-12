bfe2cded086df6b8b43ddd341d749bc14b27e6a5
Justin Nelson <justin.john.nelson@gmail.com>
2014-02-11 21:35:40 -0600

    B&C with inserts

--- B_C - Knockout Intro/Scripts/Cities.js
+++ B_C - Knockout Intro/Scripts/Cities.js
@@ -3,32 +3,33 @@ function Cities() {
     var self = this;
     
     self.allCities = ko.observableArray([]);
-
     self.allCountries = ko.observableArray([]);
 
-    self.selectedCountry = ko.observable({});
-    self.newCityName = ko.observable();
+    self.selectedCountry = ko.observable();
 
-    self.newCity = function () {
-        $.ajax({
-            type: 'POST',
-            dataType: 'json',
-            url: '/v1/Cities',
-            contentType: "application/json",
-            data: JSON.stringify({
-                Name: self.newCityName(),
-                CountryCode: self.selectedCountry().Code
-            }), 
-            success: function () { }
-        });
-    };
+    self.newCityName = ko.observable();
 
     self.loadingCities = ko.computed(function () {
         return self.allCities().length == 0;
     });
-    self.loadingCountries = ko.computed(function () {
-        return self.allCountries().length == 0;
-    });
+
+    self.newCity = function () {
+        $.ajax({
+            type: 'Post',
+            dataType: 'json',
+            url: '/v1/Cities',
+            contentType: 'application/json',
+            data: JSON.stringify({
+                Name: self.newCityName(),
+                CountryCode: self.selectedCountry().Code
+            }),
+            success: function () {
+                self.allCities.push({
+                    Name: self.newCityName()
+                });
+            }
+        });
+    };
 
     $.get('/v1/Cities', function (data) {
         self.allCities(data);
